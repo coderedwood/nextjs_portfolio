208487a0aef07515fd61a1fdebc0c0e217c2887a
Dwayne Redwood <dwayneredwood@live.com>
2024-04-14 16:01:06 +0000

    added social links to menu overlay for mobile

--- portfolio-website/app/components/Navbar.jsx
+++ portfolio-website/app/components/Navbar.jsx
@@ -84,7 +84,7 @@ const Navbar = () => {
           </ul>
         </div>
         </div>
-        {navbarOpen?<MenuOverlay links={navLinks}/>:null}
+        {navbarOpen?<MenuOverlay links={navLinks} sLinks={socialLinks}/>:null}
     </nav>
   )
 }
