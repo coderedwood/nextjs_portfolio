0d6d13b8a0ecf877e7fa3898c20e78fad4002ef8
Dwayne Redwood <dwayneredwood@live.com>
2024-04-21 19:30:33 +0000

    styled about skills section. added svg component

--- portfolio-website/app/components/AboutSkillSection.jsx
+++ portfolio-website/app/components/AboutSkillSection.jsx
@@ -1,9 +1,19 @@
 'use client'
 import React from 'react'
+import SkillsSVG from './subcomponents/SkillsSVG'
 
 const AboutSkillSection = () => {
   return (
-    <section>AboutSkillSection</section>
+    <section className='content-center'>
+    <div className='text-center py-4 mt-16'>
+        <h1 className='mb-4 text-4xl lg:text-5xl xl:text-[80px] font-extrabold'>
+            <span className={`text-black`}>SKILLS</span>
+        </h1>
+    </div>
+    <div className=' ml-auto mr-auto lg:w-[55vw]'>
+      <SkillsSVG />
+    </div>
+    </section>
   )
 }
 
