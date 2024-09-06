1b65b0efc28dccfcae82e68b207e81cdf29e9af7
Dwayne Redwood <dwayneredwood@live.com>
2024-09-06 21:36:05 +0000

    updated styling for projects section

--- portfolio-website/app/components/ProjectsSection.jsx
+++ portfolio-website/app/components/ProjectsSection.jsx
@@ -11,7 +11,7 @@ const ProjectsSection = ({imageUrl, title, description, gitUrl, projectUrl}) => 
             <div className='h-[100%] pt-[5%] items-center'>
                 <div className='flex flex-col w-[100%] pt-[5%] pr-[5%] pb-[5%] pl-[10%]'>
                     <div className='text-6xl font-bold'><span>Old Harbour News</span></div>
-                    <div className='text-xl font-semibold pt-5'><span>Website Admin & Support</span></div>
+                    <div className='text-2xl font-semibold pt-5'><span>Website Admin &nbsp; • &nbsp; Support</span></div>
                     <div className='text-xl pt-2'><span>A news site that is dedicated to providing news about old harbour and other areas of jamaica to jamaica and the jamaican diaspora</span></div>
                 </div>
             </div>
