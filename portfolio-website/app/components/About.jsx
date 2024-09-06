a006f381ea9bee4a1141c1c7553fd9216a839353
Dwayne Redwood <dwayneredwood@live.com>
2024-09-06 21:00:50 +0000

    refactor, clean up and projects rework

--- portfolio-website/app/components/About.jsx
+++ portfolio-website/app/components/About.jsx
@@ -1,61 +1,10 @@
 "use client";
 import React from 'react';
-// import React, { useTransition, useState } from 'react';
-// import Image from 'next/image';
-// import TabButton from './TabButton';
 import AboutBiographySection from './AboutBiographySection';
 import AboutSkillSection from './AboutSkillSection';
 import AboutInterestSection from './AboutInterestSection';
 
-{/*const TAB_DATA = [
-    {
-        title: "Skills",
-        id: "skills",
-        content: (
-            <ul className='list-disc pl-2'>
-                <li>NextJS</li>
-                <li>Python</li>
-                <li>Oracle SQL</li>
-                <li>MongoDB</li>
-                <li>Javascript</li>
-                <li>.Net</li>
-            </ul>
-        )
-    },
-    {
-        title: "Education",
-        id: "education",
-        content: (
-            <ul className='list-disc pl-2'>
-                <li>Microsoft Learn</li>
-                <li>University of the West Indies, Mona Campus, Jamaica, West Indies.</li>
-            </ul>
-        )
-    },
-    {
-        title: "Certifications",
-        id: "certifications",
-        content: (
-            <ul className='list-disc pl-2'>
-                <li>
-                Microsoft Certified: Azure Fundamentals
-                </li>
-            </ul>
-        )
-    }
-]
-*/}
-
 const About = () => {
-    {/*const [tab, setTab] = useState("Skills");
-    const [isPending, startTransition] = useTransition();
-
-    const handleTabChange = (id) => {
-        startTransition(()=>{
-            setTab(id);
-        });
-    };
-*/}
   return (
     <>
         <AboutBiographySection />
---
+++ portfolio-website/app/components/ProjectsSection.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Link from 'next/link';
+
+const ProjectsSection = ({imageUrl, title, description, gitUrl, projectUrl}) => {
+  return (
+    <div className='flex flex-row'>
+        <section className='flex flex-col top-0 left-0 fixed text-black h-[100vh] w-[50%]'>
+            <div className='p-[5%] block text-2xl font-semibold box-border'>
+                <span>01</span>
+            </div>
+            <div className='h-[100%] pt-[5%] items-center'>
+                <div className='flex flex-col w-[100%] pt-[5%] pr-[5%] pb-[5%] pl-[10%]'>
+                    <div className='text-6xl font-bold'><span>Old Harbour News</span></div>
+                    <div className='text-xl font-semibold pt-5'><span>Website Admin & Support</span></div>
+                    <div className='text-xl pt-2'><span>A news site that is dedicated to providing news about old harbour and other areas of jamaica to jamaica and the jamaican diaspora</span></div>
+                </div>
+            </div>
+            <div className='text-xl font-semibold p-[5%]'>
+                <span>Website</span>
+            </div>
+        </section>
+        <div className='flex flex-col ml-[50%] w-[50%] h-[925vh]'>
+            <img src='/images/projects/OHN.png'/>
+        </div>
+    </div>
+  );
+};
+
+export default ProjectsSection;
