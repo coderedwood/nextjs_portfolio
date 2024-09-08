27d681cff5d066199f2c670b56f19f3b867a1caf
Dwayne Redwood <dwayneredwood@live.com>
2024-09-08 16:19:20 -0500

    fixing textcontent

--- portfolio-website/app/components/ProjectsSection.jsx
+++ portfolio-website/app/components/ProjectsSection.jsx
@@ -105,27 +105,25 @@ const projectData = [
 //     );
 // };
 
-const ProjectsSection = ({imageUrl, title, roles, description, cType, gitUrl, projectUrl, bulletNumber}) => {
+const ProjectsSection = () => {
   return (
     <div className='flex flex-row'>
-        <section className='flex flex-col top-0 left-0 fixed text-black h-[100vh] w-[50%]'>
-            <div className='p-[5%] block text-2xl font-semibold box-border'>
-                <span>{projectData[1].bullletText}</span>
-            </div>
-            <div className='h-[100%] pt-[5%] items-center'>
-                <div className='flex flex-col w-[100%] pt-[5%] pr-[5%] pb-[5%] pl-[10%]'>
-                    <div className='text-6xl font-bold'><span>{projectData[1].title}</span></div>
-                    <div className='text-2xl font-semibold pt-5'><span>{projectData[1].role}</span></div>
-                    <div className='text-xl pt-2'><span>{projectData[1].description}</span></div>
-                </div>
-            </div>
-            <div className='text-xl font-semibold p-[5%]'>
-                <span>{projectData[1].contentType}</span>
-            </div>
-        </section>
-        <div className='flex flex-col ml-[50%] w-[50%] h-[925vh]'>
-            <img src={projectData[1].image}/>
-        </div>
+        {projectData.map(project => (
+        <>
+            <TextContent
+            key={project.id}
+            bullletText={project.bullletText}
+            title={project.title}
+            role={project.role}
+            description={project.description}
+            contentType={project.contentType}
+        />
+        {/* <div className='flex flex-col ml-[50%] w-[50%] h-[925vh]'>
+            <img src={project.image}/>
+        </div> */}
+        </>
+        ))
+        }
     </div>
   );
 };
